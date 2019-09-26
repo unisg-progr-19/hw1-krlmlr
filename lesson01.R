8e4c887a09e758db53016ecd8ad825e83a46f8c5
Kirill Müller <krlmlr@users.noreply.github.com>
2019-09-26 16:44:20 +0200

    Add two more plots, render

--- lesson01.R
+++ lesson01.R
@@ -35,6 +35,12 @@ view(mpg)
 ggplot(mpg, aes(x = displ, y = hwy)) +
   geom_point()
 
+ggplot(mpg, aes(x = model, y = hwy)) +
+  geom_point()
+
+ggplot(mpg, aes(x = model, y = hwy)) +
+  geom_boxplot()
+
 #' 3. Bonus: Read about "GitHub Pages". Create a new directory `docs/`,
 #'    save the 'knitted' result of your work in this directory as `index.html`
 #'    and publish it so that others can see your work in a browser.
