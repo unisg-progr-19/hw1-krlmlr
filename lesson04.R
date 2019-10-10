7fffeaa86a60e8b532c472bbd27f5ad01999ea8b
Kirill Müller <krlmlr@users.noreply.github.com>
2019-10-10 22:11:39 +0200

    Adding file: lesson04.R

---
+++ lesson04.R
@@ -0,0 +1,45 @@
+#' ## Assignment Set 1: Lesson 4
+#'
+#' Add R Code under the questions.
+#' If you are done, 'knit' the document, commit and push to GitHub.
+#' You can use GitHub Pages if you like, add links to README.md
+#' to make it easier to find the pretty version.
+#'
+#' 1. Summaries
+#'
+#' Solve your choice of 8 exercises at
+#'
+#' https://krlmlr.github.io/vistransrep/2019-09-unisg/dplyr.html
+#'
+#' "Group operations", sections 1-2.
+#'
+#' The website already includes solutions to the exercises.
+#' Therefore, please include an explanation what happens in your code
+#' as a comment.
+#'
+#' Resources:
+#'
+#' - https://r4ds.had.co.nz/transform.html, Section 5.6
+#' - https://krlmlr.github.io/vistransrep/book/transformation.html, 3.7
+#' - https://github.com/rstudio/cheatsheets/raw/master/data-transformation.pdf
+#'
+#' 2. Layers with statistical transformations
+#'
+#' Use the `mpg` data or a dataset of your choice to create plots that
+#' display the data in some aggregated form.
+#' See the slides at the end of
+#' https://unisg-progr-19.github.io/slides/04-transformation-2.html
+#' (histogram, smoothing lines, boxplots) for examples.
+#' For one of the plots, compute the aggregation manually using
+#' group_by(), summarize() and ungroup() and use a different geom
+#' or stat = "identity".
+#'
+#' Resources:
+#'
+#' - https://ggplot2.tidyverse.org/reference/index.html, Section
+#'   "Layer: geoms"
+#' - https://r4ds.had.co.nz/data-visualisation.html, Section 3.7
+#'
+#' Bonus: Review the "hw1-krlmlr" repository for a setup where all "knitted"
+#'        documents automatically end up in the `docs/` directory.
+#'        Apply this setup to your repository.
